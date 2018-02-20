4e44d53140508156b55e088710639f522661e12f
Dennis <dennisek85@gmail.com>
2018-02-20 14:35:23 +0100

    Basic  filter functions

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const movies = require('./movies.json');
+
+const app = express();
+
+/* let routeInfo = {
+    '/movie/title/:title': (title) => movie.title == req.params.title,
+    '/movie/year/:year': (year) => movie.year == req.params.year
+};
+
+for(let route in routeInfo){
+    app.get(route, (req,res) => {
+        let chosen = movies.filter(routeInfo[route]);
+        res.json(chosen);
+    }
+)} */
+
+
+app.get('/movie/title/:title', (request,response) =>{
+    let chosen = movies.filter((movie)=>{
+        return movie.title == request.params.title;
+    })
+    response.json(chosen);
+})
+
+app.get('/movie/year/:year', (request,response) =>{
+    let chosen = movies.filter((movie)=>{
+        return movie.year == request.params.year;
+    })
+    response.json(chosen);
+})
+
+app.get('/movie/genre/:genre', (request,response) =>{
+    let chosen = movies.filter((movie)=>{
+        return movie.genre == request.params.genre;
+    })
+    response.json(chosen);
+})
+
+app.get('*', (request, response) =>{
+    response.json('Hello');
+})
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000');
+}) 
